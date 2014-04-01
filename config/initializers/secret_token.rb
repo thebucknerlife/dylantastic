9e1badeeec8d4b8b32cebe08e67a344692fbf03c
Greg Buckner <gbuckner23@gmail.com>
2014-03-31 18:43:29 -0700

    Commited the rest of the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dylanastic::Application.config.secret_key_base = 'f2444d192c2af2d664eaada46e3a281b47eb03c6ccc56f6ec466079be60de04642f9fcf4dc76abec820f67b6211179d17ae0ee4aca798ccfcbafc19807b899be'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dylanastic::Application.config.session_store :cookie_store, key: '_dylanastic_session'
